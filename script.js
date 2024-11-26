b230376197d52b615fb5b69336a94ebd5173cf11
Ethan Legum <71243839+elegs99@users.noreply.github.com>
2024-11-26 18:51:20 -0500

    Bug With Typed Placeholder

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ $(document).ready(function(){
     });
 
     // Typing animation setup
-    new Typed(".typing", {
-        strings: ["a Software Engineer.", "a Problem Solver.", "your Next Hire.^5000"],
+    /*new Typed(".typing", {
+        strings: ["a Problem Solver.", "your Next Hire.^5000"],
         typeSpeed: 60,
         backSpeed: 40,
+        startDelay: 1000,
         loop: true
-    });
+    });*/
+    setTimeout(() => {
+        var typed = new Typed('.typing', {
+          strings: ["a Software Engineer.", "a Problem Solver.", "your Next Hire.^2000"],
+          loop: true,
+          typeSpeed: 60,
+          smartBackspace: true,
+          backSpeed: 40,
+          onLastStringBackspaced: () => typed.strPos = 1
+        });
+      }, 750)
 });
